00a01f92ac7dac79b73f64ef6cf05ac49828e067
JCMondragon7 <johnathan12907@gmail.com>
2025-12-11 15:14:42 +0000

    loopy woopy woop

--- 4_problem_set_1.py
+++ 4_problem_set_1.py
@@ -104,6 +104,17 @@ def carprice(n):
         return carprice(n - 1) + carprice(n - 2)
 print(carprice(6)) #output 8
 
+def fibonacci(n):
+
+    if n in [1,2]:
+        return 1
+    
+    return fibonacci(n-1)+fibonacci(n+2)
+
+for i in range(1,10):
+    print(fibonacci(i))
+
+
 
 # ### **Problem 10: Pattern Printing**
 
